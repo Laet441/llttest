b27630f15679beafdcfd1dc3705421cb76c8d46e
Laet441 <lightdeveloperru@gmail.com>
2025-07-17 16:17:05 +0300

    a

---
+++ connect.js
@@ -0,0 +1,16 @@
+export function connect() {
+  require("dotenv").config();
+
+  // let gh = "https://laet441.github.io/llttest/";
+  let db = "https://loett-f231.restdb.io/rest/test";
+
+  fetch(db, { headers: {
+    'Content-Type': 'application/json',
+    'cache-control': 'no-cache',
+    'x-apikey': process.env.API_KEY
+  } })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
+}
+
--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-require("dotenv").config();
-
-// let gh = "https://laet441.github.io/llttest/";
-let db = "https://loett-f231.restdb.io/rest/test";
-
-fetch(db, { headers: {
-  'Content-Type': 'application/json',
-  'cache-control': 'no-cache',
-  'x-apikey': process.env.API_KEY
-} })
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error))
+import { connect } from "./connect";
+connect();
 
 // console.log(process.env);//.API_KEY);
 // 
