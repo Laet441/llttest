4fa1cc5bf24c874b11e455965c85116be428c6a9
Laet441 <lightdeveloperru@gmail.com>
2025-07-17 15:37:04 +0300

    a

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 // let gh = "https://laet441.github.io/llttest/";
 let db = "https://loett-f231.restdb.io/rest/test";
 
-// fetch(gh)
-// .then(response => response.json())
-// .then(data => console.log(data))
-// .catch(error => console.error(error))
+fetch(db, { headers: {
+  'Content-Type': 'application/json',
+  'cache-control': 'no-cache',
+  'x-apikey': process.env.API_KEY
+} })
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error))
 
 console.log(process.env);//.API_KEY);
 // 
