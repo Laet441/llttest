7e01d06628d5a9420e5d511ec6ce62bd00e0456e
Laet441 <lightdeveloperru@gmail.com>
2025-07-17 16:26:19 +0300

    a

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ console.log(process.env.API_KEY);
 // 
 // console.log("BRUH");
 
+// let gh = "https://laet441.github.io/llttest/";
+// let db = "https://loett-f231.restdb.io/rest/test";
+
+// fetch(db, { headers: {
+//   'Content-Type': 'application/json',
+//   'cache-control': 'no-cache',
+//   'x-apikey': process.env.API_KEY
+// } })
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .catch(error => console.error(error))
+
