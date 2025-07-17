9ef60532e16d5040c3c393e9c90113c8bdeb63be
Laet441 <lightdeveloperru@gmail.com>
2025-07-17 16:59:35 +0300

    a

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // import { connect } from "./connect.js";
 // connect();
 
-import { dotenv } from "dotenv";
-dotenv.config();
-  
+require("dotenv").config();
+
 console.log(process.env.API_KEY);
 
 // console.log(process.env);//.API_KEY);
